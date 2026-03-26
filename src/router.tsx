97802be0952f3aa1ea9adca094332cdf5b407fa5
FatlirT <fatlir-t@outlook.com>
2026-03-26 20:56:38 +0000

    feat: add auth directory to contain all auth artifacts

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { TaskList, TaskNew, TaskView } from "./pages/tasks";
+import { TaskList, TaskNew, TaskView } from "./tasks/pages";
+import { SignIn } from "./auth/pages";
 
 export default function Router() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/sign-in" />} />
+
+      <Route path="/sign-in" element={<SignIn />} />
+
       <Route path="/tasks/:title" element={<TaskView />} />
       <Route path="/tasks/new" element={<TaskNew />} />
       <Route path="/tasks" element={<TaskList />} />
